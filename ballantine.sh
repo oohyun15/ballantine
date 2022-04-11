a69070dab90f3656abcdca205a685fe607da233d
oohyun15 <sakiss4774@gmail.com>
2022-04-12 01:40:17 +0900

    :sparkles: validate .git

--- ballantine.sh
+++ ballantine.sh
@@ -6,7 +6,6 @@ RED='\033[1;31m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
-APP_NAME=$(basename -s .git `git config --get remote.origin.url`)
 
 # function definition start
 
@@ -25,8 +24,20 @@ function init() {
   touch ~/.blntrc
   echo "target_branch=$target_branch" >> ~/.blntrc
   echo "webhook=$webhook" >> ~/.blntrc
+}
 
-  exit 0
+function manual() {
+  echo "[commands]"
+  echo "ballantine init               : initialize ballantine configuration"
+  echo "ballantine                    : check commits between \$target_branch and \$current_branch."
+  echo "ballantine production main    : check commits between \`production\` and \`main\` branch."
+  echo "ballantine -s production main : send to slack commits between \`production\` and \`main\` branch."
+
+  echo
+  echo "[options]"
+  echo "-h: help"
+  echo "-o: check ballantine option values."
+  echo "-s: send to slack using slack webhook URL."
 }
 
 # reference: https://github.com/desktop/desktop/blob/a7bca44088b105a04714dc4628f4af50f6f179c3/app/src/lib/remote-parsing.ts#L27-L44
@@ -199,25 +210,17 @@ function send_to_terminal () {
 
 # function definition end
 
-if [[ $1 == 'init' ]]; then init; fi
+if [[ $1 == 'init' ]]; then
+  init
+  exit 0
+fi
 
-source ~/.blntrc
-
+source ~/.blntrc &> /dev/null
 type='terminal'
 while getopts "hos" opt; do
   case $opt in
     h)
-      echo "[commands]"
-      echo "ballantine init               : initialize ballantine configuration"
-      echo "ballantine                    : check commits between \$target_branch and \$current_branch."
-      echo "ballantine production main    : check commits between \`production\` and \`main\` branch."
-      echo "ballantine -s production main : send to slack commits between \`production\` and \`main\` branch."
-
-      echo
-      echo "[options]"
-      echo "-h: help"
-      echo "-o: check ballantine option values."
-      echo "-s: send to slack using slack webhook URL."
+      manual
       exit 0
       ;;
     o)
@@ -234,11 +237,17 @@ while getopts "hos" opt; do
   esac
 done
 
+if ! [ -f ".git" ]; then
+  echo "ERROR: There is no \".git\" in `pwd` directory."
+  exit 1
+fi
+
+APP_NAME=$(basename -s .git `git config --get remote.origin.url`)
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 if [ -n "$1" ]; then from=$1; else from=$target_branch; fi
 if [ -n "$2" ]; then to=$2; else to=$current_branch; fi
 if [ -z "$from" ]; then
-  echo "target branch is required. run \`ballantine init\` or set target branch to argument."
+  echo "ERROR: target branch is required. run \`ballantine init\` or set target branch to argument."
   exit 1
 fi
 diff_commits $type $from $to
