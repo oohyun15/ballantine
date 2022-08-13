3f0b62d7ceca4e0e52b8dff6176a2f47c17d66fc
oohyun15 <sakiss4774@gmail.com>
2022-08-14 05:24:58 +0900

    :sparkles: Add method: send_commits

--- ballantine.rb
+++ ballantine.rb
@@ -4,13 +4,15 @@ require 'thor'
 class Ballantine < Thor
   FILE_GITMODULES = '.gitmodules'
 
-  P_NC='\033[0m'
-  P_GRAY='\033[1;30m'
-  P_RED='\033[1;31m'
-  P_GREEN='\033[1;32m'
-  P_YELLOW='\033[1;33m'
-  P_BLUE='\033[1;34m'
-  P_CYAN='\033[1;36m'
+  P_NC= "\e[0m"
+  P_GRAY= "\e[1;30m"
+  P_RED= "\e[1;31m"
+  P_GREEN= "\e[1;32m"
+  P_YELLOW= "\e[1;33m"
+  P_BLUE= "\e[1;34m"
+  P_CYAN= "\e[1;36m"
+
+  TEMP_PATH = '/tmp/commit_log'
 
   # reference: https://github.com/desktop/desktop/blob/a7bca44088b105a04714dc4628f4af50f6f179c3/app/src/lib/remote-parsing.ts#L27-L44
   GITHUB_REGEXES = [
@@ -35,6 +37,7 @@ class Ballantine < Thor
   # @param [String] to
   def diff(from, to)
     @_options = options
+    @app_name = `git config --get remote.origin.url`.chomp[/([\w-]+)\.git/, 1]
 
     # check argument is tag
     from = check_tag(from)
@@ -44,7 +47,7 @@ class Ballantine < Thor
     system 'git pull -f &> /dev/null'
 
     # find main, sub path
-    @temp_path = Dir.mktmpdir
+    @temp_path = Dir.mkdir(TEMP_PATH) rescue TEMP_PATH
     @main_path = Dir.pwd
     @sub_path =
       if Dir[FILE_GITMODULES].any?
@@ -75,11 +78,7 @@ class Ballantine < Thor
       system "cd #{@main_path}"
     end
 
-    number = Dir[@temp_path+'/*'].size
-    if number.zero?
-      puts "ERROR: There is no commits between \"#{from}\" and \"#{to}\""
-      exit 1
-    end
+    send_commits(from, to, main_url)
   end
 
   private
@@ -103,8 +102,8 @@ class Ballantine < Thor
       file_path = @temp_path + "/#{author}.log"
       format = commit_format(url)
       commits = `git --no-pager log --reverse --no-merges --author=#{author} --format="#{format}" --abbrev=7 #{from}..#{to}`.gsub('"', '\"')
-      next unless commits.any?
-      count = commits.size
+      next if commits.empty?
+      count = commits.split("\n").size
       var = count == 1 ? 'commit' : 'commits'
 
       file = File.open(file_path, 'a')
@@ -128,7 +127,9 @@ class Ballantine < Thor
   def github_url(url)
     owner, repository =
       GITHUB_REGEXES.each do |regex|
-        break [str[2], str[3]] if (str = url.match(regex))
+        if (str = url.match(regex))
+          break [str[2], str[3]]
+        end
       end
     "https://github.com/#{owner}/#{repository}"
   end
@@ -157,6 +158,27 @@ class Ballantine < Thor
       "- #{P_YELLOW}%h#{P_NC} %s #{P_GRAY}#{url}/commit/%H#{P_NC}"
     when TYPE_SLACK
       "\`<#{url}/commit/%H|%h\` %s - %an"
+    end
+  end
+
+  # @param [String] from
+  # @param [String] to
+  # @param [String] url
+  def send_commits(from, to, url)
+    number = Dir[@temp_path+'/*'].size
+    if number.zero?
+      puts "ERROR: There is no commits between \"#{from}\" and \"#{to}\""
+      exit 1
+    end
+    last_commit = `git --no-pager log --reverse --format="#{commit_format(url)}" --abbrev=7 #{from}..#{to} -1`
+
+    case @_options[:type]
+    when TYPE_TERMINAL
+      puts "Check commits before #{P_RED}#{@app_name}#{P_NC} deployment."
+      puts "(#{P_CYAN}#{from}#{P_NC} <- #{P_CYAN}#{to}#{P_NC}) #{P_GRAY}#{url}/compare/#{from}...#{to}#{P_NC}"
+      puts "#{P_YELLOW}Author#{P_NC}: #{number}"
+      puts "#{P_BLUE}Last Commit#{P_NC}: #{last_commit}"
+    when TYPE_SLACK
     end
   end
 end
