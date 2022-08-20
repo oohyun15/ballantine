47aa0d85bbda55c45a19aad30749024db8f6052e
oohyun15 <sakiss4774@gmail.com>
2022-08-20 19:57:42 +0900

    :sparkles: Add more errors

--- ruby/lib/ballantine.rb
+++ ruby/lib/ballantine.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
+require 'thor'
+require 'json'
 require_relative "ballantine/cli"
 require_relative "ballantine/version"
 require_relative 'ballantine/author'
 require_relative 'ballantine/string'
+require_relative 'ballantine/config'
 
 module Ballantine
   class Error < StandardError; end
+  class NotAllowed < Error; end
+  class AssertionFailed < Error; end
+  class NotImplemented < Error; end
 end
