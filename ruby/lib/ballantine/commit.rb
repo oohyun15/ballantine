3af7fc3caa394dbf1f3560e77becb89a03eba460
oohyun15 <sakiss4774@gmail.com>
2023-01-25 00:32:47 +0900

    :recycle: Refactor check commits

--- ruby/lib/ballantine/commit.rb
+++ ruby/lib/ballantine/commit.rb
@@ -2,27 +2,33 @@
 
 module Ballantine
   class Commit
-    attr_reader :hash # attributes
+    attr_reader :hash, :message # attributes
     attr_reader :repo, :author # associations
 
     class << self
       # @param [String] hash
       # @param [Repository] repo
+      # @param [String] message
+      # @param [Author, NilClass] message
       # @return [Commit]
-      def find_or_create_by(hash:, repo:)
+      def find_or_create_by(hash:, repo:, message: nil, author: nil)
         @_collections = {} unless defined?(@_collections)
         index = "#{hash}-#{repo.name}"
         return @_collections[index] unless @_collections[index].nil?
 
-        @_collections[index] = new(hash:, repo:)
+        @_collections[index] = new(hash:, repo:, message:, author:)
       end
     end
 
     # @param [String] hash
     # @param [Repository] repo
-    def initialize(hash:, repo:)
+    # @param [String] message
+    # @param [Author] author
+    def initialize(hash:, repo:, message:, author:)
       @hash = hash
       @repo = repo
+      @message = message
+      @author = author
     end
   end
 end
