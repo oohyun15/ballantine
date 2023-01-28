adbdd8b8a1febfe844d69b39dd8ceba9c9f0e054
oohyun15 <sakiss4774@gmail.com>
2023-01-29 01:18:15 +0900

    :truck: Move source code to root

--- ruby/lib/ballantine.rb
+++ ruby/lib/ballantine.rb
@@ -3,13 +3,13 @@
 require "thor"
 require "json"
 require_relative "string"
+require_relative "printable"
 require_relative "ballantine/version"
 require_relative "ballantine/config"
 require_relative "ballantine/author"
 require_relative "ballantine/repository"
 require_relative "ballantine/commit"
 require_relative "ballantine/cli"
-require_relative "ballantine/printable"
 
 module Ballantine
   class Error < StandardError; end
