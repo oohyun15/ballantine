cd76fb0618230f4fada6121ae8f7c5c6b800db47
oohyun15 <sakiss4774@gmail.com>
2023-01-29 03:16:13 +0900

    :wrench: Update commit attributes

--- ruby/lib/ballantine/commit.rb
+++ ruby/lib/ballantine/commit.rb
@@ -2,38 +2,37 @@
 
 module Ballantine
   class Commit
-    attr_reader :hash, :long_hash, :subject, :url # attributes
+    attr_reader :hash, :long_hash, :subject # attributes
     attr_reader :repo, :author # associations
 
     class << self
       # @param [String] hash
-      # @param [String] long_hash
-      # @param [String] subject
       # @param [Repository] repo
-      # @param [Author, NilClass] subject
       # @return [Commit]
-      def find_or_create_by(hash:, long_hash: nil, subject: nil, repo:, author: nil)
+      def find_or_create_by(hash:, repo:)
         @_collections = {} unless defined?(@_collections)
         index = "#{hash}-#{repo.name}"
         return @_collections[index] unless @_collections[index].nil?
 
-        @_collections[index] = new(hash:, long_hash:, subject:, repo:, author:)
+        @_collections[index] = new(hash:, repo:)
       end
     end
 
     # @param [String] hash
-    # @param [String] long_hash
-    # @param [String] subject
     # @param [Repository] repo
-    # @param [Author] author
-    def initialize(hash:, long_hash:, subject:, repo:, author:)
+    def initialize(hash:, repo:)
       @hash = hash
-      @long_hash = long_hash
-      @subject = subject
       @repo = repo
-      @author = author
-      @url = "#{repo.url}/commit/#{long_hash}"
     end
+
+    # @return [Commit]
+    def update(**kwargs)
+      # TODO: validate keys and values
+      kwargs.each { |key, value| instance_variable_set("@#{key}", value) }
+      self
+    end
+
+    def url; @url ||= "#{repo.url}/commit/#{long_hash}" end
 
     # @return [String]
     def slack_message
