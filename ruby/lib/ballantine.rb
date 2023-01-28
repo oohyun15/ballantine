bf85ae26fa6130991003d06c30ef0aaa226e2ca1
oohyun15 <sakiss4774@gmail.com>
2023-01-29 00:20:44 +0900

    :tada: Initialize module: Printable

--- ruby/lib/ballantine.rb
+++ ruby/lib/ballantine.rb
@@ -9,6 +9,7 @@ require_relative "ballantine/author"
 require_relative "ballantine/repository"
 require_relative "ballantine/commit"
 require_relative "ballantine/cli"
+require_relative "ballantine/printable"
 
 module Ballantine
   class Error < StandardError; end
